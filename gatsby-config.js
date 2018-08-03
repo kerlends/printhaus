c26dd851eb475cc4f56890509db08021016a1acc
Konrad Erlendsson <konrade@gmail.com>
2018-08-03 06:28:46 -0400

    manifest display change

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
         start_url: '/',
         background_color: '#252525',
         theme_color: '#252525',
-        display: 'standalone',
+        display: 'minimal-ui',
         icon: 'src/images/icon-125.png',
       },
     },
